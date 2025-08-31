7491aca9cab3eb20390b4c48fc72f0932a32127a
Sayani Roy Choudhury <leosubho88@gmail.com>
2025-08-31 14:04:24 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,8 @@
-export default async function handler(req, res) {
-  try {
+export default async function handler(req, res) {  try {
     if (req.method === "POST") {
       console.log("Incoming Webhook Body:", req.body);
-      res.status(200).json({ success: true, echo: req.body });
+
+       res.status(200).json({ success: true, echo: req.body });
     } else {
       res.status(200).send("Webhook running OK (GET)");
     }
